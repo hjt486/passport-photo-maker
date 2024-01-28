5c007dbf9cb89fd1ff8390fb00a6be5c167fc87c
hjt486 <hjt486@gmail.com>
2024-01-28 10:05:16 -0600

    updated date privacy policy

--- src/App.js
+++ src/App.js
@@ -839,8 +839,12 @@ const Disclaimer = ({
     <>
       <dialog open={modals.disclaimer} className='modal'>
         <article>
-          <h4>{translate("disclaimerTitle")}</h4>
+          <h4>{translate("disclaimerModalTitle")}</h4>
+          <small><b>{translate("disclaimerTitle")}</b></small>
           <small>{translate("disclaimer")}</small>
+          <br/>
+          <small><b>{translate("datePrivacyTitle")}</b></small>
+          <small>{translate("disclaimer2")}</small>
           <footer>
             <button onClick={() => setModals((prevModals) => ({ ...prevModals, disclaimer: false }))}>{translate("agreeButton")}</button>
             <button onClick={() => {window.location.href = 'https://www.google.com';}}>{translate("disagreeButton")}</button>
