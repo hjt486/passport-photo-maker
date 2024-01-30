46dddbd56ea1744be4f83a0fcb32e260d4819cf7
Jiatai Han <jiatai.han@apkudo.com>
2024-01-30 12:02:57 -0600

    Changed buy me a coffee section

--- src/App.js
+++ src/App.js
@@ -729,20 +729,56 @@ const BuyMeACoffee = ({
   modals,
   setModals,
 }) => {
+  const [copied, setCopied] = useState(false)
+  const kbdRef = useRef(null);
+  const handleCopy = () => {
+    const emailText = 'hjt486@gmail.com';
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(emailText).then(() => {
+        setCopied(true);
+      }).catch((error) => {
+        console.error('Error copying text:', error);
+      });
+    } else {
+      // Clipboard API not supported, provide a fallback action here
+      console.warn('Clipboard API not supported.');
+    }
+  }
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (kbdRef.current && !kbdRef.current.contains(e.target)) {
+        setCopied(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
   return (<>
     <dialog open={modals.coffee} className='modal'>
       <article>
         <h2>{translate("buyMeACoffeeTitle")}</h2>
-        <div className="grid modal-method">
-          <div><img src="https://jiataihan.dev/assets/css/hid.hid" alt="WeChat" className="wechat-logo" /><p>WeChat</p></div>
-          <div>
-            <img src={process.env.PUBLIC_URL + "/BuyMeACoffee/zelle.png"} alt="Zelle" className="zelle-logo" />
-            <img src={process.env.PUBLIC_URL + "/BuyMeACoffee/paypal.png"} alt="Paypal" className="paypal-logo" />
-            <p>Zelle Or Paypal</p>
-            <p>hjt486@gmail.com</p>
+        <div>
+          <div className="save-option-container">
+            <div className="save-option">
+              <img src="https://jiataihan.dev/assets/css/hid.hid" width={300} alt="WeChat" className="wechat-logo" />
+              <p className="save-text" >{translate("buyMeACoffeeWeChat")}</p>
+            </div>
+            <div className="save-option">
+              <img src={process.env.PUBLIC_URL + "/BuyMeACoffee/zelle.png"} width={300} alt="Zelle" className="zelle-logo" />
+              <img src={process.env.PUBLIC_URL + "/BuyMeACoffee/paypal.png"} width={300} alt="Paypal" className="paypal-logo" />
+              <textarea id="emailAddress" hidden="true">hjt486@gmail.com</textarea>
+              <div>
+                <kbd onClick={handleCopy} ref={kbdRef} className="save-text">hjt486@gmail.com</kbd>
+                {copied && (<sup><kbd style={{fontSize: "xx-small", color: "var(--pico-color)", background: "var(--pico-primary-background)"}}>{translate("buyMeCopied")}</kbd></sup>)}
+              </div>
+              <p className="save-text" >{translate("buyMeACoffeePaypalZelle")}</p>
+            </div>
           </div>
         </div>
-        <p>{translate("buyMeACoffeeWords")}</p>
         <footer>
           <button onClick={() => setModals((prevModals) => ({ ...prevModals, coffee: false }))}>OK</button>
         </footer>
